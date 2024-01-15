3d8132d8c0032b96729d75d427ad11f5f531c4e0
Franz Manrique <franzmanrique2121@gmail.com>
2024-01-15 10:25:08 +0800

    installed vue.js and bootstrap ui

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,16 @@
 import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue"; //add this line
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: [
                 'resources/sass/app.scss',
-                'resources/sass/nav.scss',
                 'resources/js/app.js',
-
             ],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
     ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
 });
